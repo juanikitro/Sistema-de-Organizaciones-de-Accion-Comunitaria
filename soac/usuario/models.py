776ebd8e811cec53bf4165493b25a0b0f00d3bcb
JPKitro <jpkitro@gmail.com>
2021-11-12 11:36:32 -0300

    create requirements.txt

--- soac/usuario/models.py
+++ soac/usuario/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Usuario(models.Model):
     # 1:1 con User
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # USERNAME_FIELD = 'cuit'
+    USERNAME_FIELD = 'cuit'
     
     # Importantes
     cuit = models.IntegerField(unique = True)
