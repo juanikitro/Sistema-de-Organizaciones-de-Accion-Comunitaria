f8ba8cb5b702905854d4e8b6bd396d5cf875d0ef
JPKitro <jpkitro@gmail.com>
2021-11-30 10:17:31 -0300

    presentation

---
+++ soac/static/js/js_reveal.js
@@ -0,0 +1,10 @@
+ScrollReveal().reveal('.content', {
+	delay: 100,
+	easing: 'ease-in-out',
+	origin: 'top',
+	distance: '25px',
+	duration: 500
+});
+ScrollReveal().reveal('.navbar', {
+	duration: 0
+});
