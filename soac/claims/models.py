f973cda936425eb8818df3c27d5590e721d602e1
JPKitro <jpkitro@gmail.com>
2022-01-18 07:12:53 -0300

    visual improvemets

---
+++ soac/claims/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+class Claim(models.Model):
+    created= models.DateTimeField(auto_now_add=True, null=True)
+    observation= models.CharField(max_length=255, null=False, default='0')
+
+    org = models.CharField(null=False, max_length=2500, default='0')
+    org_name = models.CharField(null=False, max_length=255, default='0')
